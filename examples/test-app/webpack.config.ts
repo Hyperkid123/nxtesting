734b06eb3367020703248407861d0f525ed0d4a0
Martin Marosi <marvusm.mmi@gmail.com>
2023-11-30 13:41:04 +0100

    build: add webpack dev server cache for e2e

--- examples/test-app/webpack.config.ts
+++ examples/test-app/webpack.config.ts
@@ -1,6 +1,9 @@
-import { composePlugins, withNx } from '@nx/webpack';
+import { composePlugins, withNx, NxWebpackExecutionContext } from '@nx/webpack';
 import { withReact } from '@nx/react';
 import { withModuleFederation } from '@nx/react/module-federation';
+import { merge } from 'webpack-merge'
+import { Configuration } from 'webpack';
+import { join } from 'path'
 
 import baseConfig from './module-federation.config';
 
@@ -8,9 +11,20 @@ const config = {
   ...baseConfig,
 };
 
+
+const withWebpackCache = (config: Configuration, { context }: NxWebpackExecutionContext): Configuration => {
+  return merge(config, {
+    cache: {
+      type: 'filesystem',
+      cacheDirectory: join(context.root, '.webpack-cache')
+    },
+  })
+}
+
 // Nx plugins for webpack to build config object from Nx options and context.
 export default composePlugins(
   withNx(),
   withReact(),
-  withModuleFederation(config)
+  withModuleFederation(config),
+  withWebpackCache
 );
